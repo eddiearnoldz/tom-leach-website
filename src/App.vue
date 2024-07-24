780fafa58d44a98ee512bd45a2dafc1e5ce77f49
eddie arnold <eddiearnoldz@mac.com>
2024-07-24 18:54:00 +0100

    harcoded the svgs into place

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, watch } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
-import SvgIcon from '@/components/SvgIcon.vue';
 
 const isMenuOpen = ref(false);
 
@@ -26,8 +25,15 @@ watch(isMenuOpen, (newValue) => {
   <header>
     <RouterLink @click="closeMenu" to="/" class="logo">TOM/LEACH</RouterLink>
     <button @click="toggleMenu" class="menu-button">
-      <SvgIcon v-if="!isMenuOpen" name="HamburgerMenuIcon" />
-      <SvgIcon v-if="isMenuOpen" name="MenuCloseIcon" />
+      <svg v-if="!isMenuOpen" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path class="line1" d="M4.96857 7.46857H24.9686" stroke="white" stroke-width="2.0" stroke-linecap="round" stroke-linejoin="round"/>
+        <path class="line2" d="M4.96857 14.9686H24.9686" stroke="white" stroke-width="2.0" stroke-linecap="round" stroke-linejoin="round"/>
+        <path class="line3" d="M4.96857 22.4686H24.9686" stroke="white" stroke-width="2.0" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>
+      <svg v-if="isMenuOpen" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path class="line1" d="M6.25 6.25L23.75 23.75" stroke="white" stroke-width="2.0" stroke-linecap="round"/>
+        <path class="line2" d="M6.25 23.75L23.75 6.25" stroke="white" stroke-width="2.0" stroke-linecap="round"/>
+      </svg>
     </button>
   </header>
   <div :class="{ 'menu-open': isMenuOpen }" class="mobile-menu">
