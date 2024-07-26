80a28e129cdc92178d5f4be26908e2ca13023dee
eddie arnold <eddiearnoldz@mac.com>
2024-07-26 10:20:45 +0100

    added aria lable for hamburger menu and meta description tag

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ watch(isMenuOpen, (newValue) => {
 <template>
   <header>
     <RouterLink @click="closeMenu" to="/" class="logo">TOM | LEACH</RouterLink>
-    <button @click="toggleMenu" class="menu-button">
+    <button @click="toggleMenu" class="menu-button" aria-label="toggle menu button">
       <svg v-if="!isMenuOpen" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path class="line1" d="M4.96857 7.46857H24.9686" stroke="white" stroke-width="2.0" stroke-linecap="round" stroke-linejoin="round"/>
         <path class="line2" d="M4.96857 14.9686H24.9686" stroke="white" stroke-width="2.0" stroke-linecap="round" stroke-linejoin="round"/>
