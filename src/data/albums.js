050e55e836ca6ab5be2d6961d8e5e29243312aca
eddie arnold <eddiearnoldz@mac.com>
2024-07-26 10:04:52 +0100

    converted images from png to webp

--- src/data/albums.js
+++ src/data/albums.js
@@ -1,247 +1,247 @@
 export const albums = [
   {
     title: "One Million Pieces | LEAP | EP",
-    image: '/assets/albums/leap_one_million_pieces.png',
+    image: '/assets/albums/leap_one_million_pieces.webp',
     filters: ['contribution_writer', 'contribution_mixer', 'contribution_producer', 'year_2022', 'genre_rock']
   },
   {
     title: "Dark Habits | LEAP | EP",
-    image: '/assets/albums/leap_dark_habits.png',
+    image: '/assets/albums/leap_dark_habits.webp',
     filters: ['contribution_writer', 'contribution_mixer', 'contribution_producer', 'year_2023', 'genre_rock']
   },
   {
     title: "One Way Out | LEAP | Single",
-    image: '/assets/albums/leap_one_way_out.png',
+    image: '/assets/albums/leap_one_way_out.webp',
     filters: ['contribution_writer', 'contribution_mixer', 'contribution_producer', 'year_2024', 'genre_rock']
   },
   {
     title: "Power Trip | LEAP | EP",
-    image: '/assets/albums/leap_power_trip.png',
+    image: '/assets/albums/leap_power_trip.webp',
     filters: ['contribution_writer', 'contribution_mixer', 'contribution_producer', 'year_2024', 'genre_rock']
   },
   {
     title: "Real Life | AntsLive | Album",
-    image: '/assets/albums/antslive_real_life.png',
+    image: '/assets/albums/antslive_real_life.webp',
     filters: ['contribution_mixer', 'contribution_mastering', 'year_2024', 'genre_rap']
   },
   {
     title: "Just a Matter of Time | AntsLive | Album",
-    image: '/assets/albums/antslive_just_a_matter_of_time.png',
+    image: '/assets/albums/antslive_just_a_matter_of_time.webp',
     filters: ['contribution_mixer', 'contribution_mastering', 'year_2023', 'genre_rap']
   },
   {
     title: "Tweakin | AntsLive | Single",
-    image: '/assets/albums/antslive_tweakin.png',
+    image: '/assets/albums/antslive_tweakin.webp',
     filters: ['contribution_mixer', 'contribution_mastering', 'year_2022', 'genre_rap']
   },
   {
     title: "Californication (Feels Like I'm Falling In Love) | Col3trane | Single",
-    image: '/assets/albums/col3trane_californication.png',
+    image: '/assets/albums/col3trane_californication.webp',
     filters: ['contribution_mixer', 'year_2021', 'genre_r&b']
   },
   {
     title: "Rabbit Head | Tribes | Album",
-    image: '/assets/albums/tribes_rabbit_head.png',
+    image: '/assets/albums/tribes_rabbit_head.webp',
     filters: ['contribution_recording-engineer', 'contribution_mixer', 'contribution_mixer', 'contribution_production', 'year_2023', 'genre_rock']
   },
   {
     title: "monochrome blue | modernlove | EP",
-    image: '/assets/albums/modernlove_monochrome_blue.png',
+    image: '/assets/albums/modernlove_monochrome_blue.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2021', 'genre_indie']
   },
   {
     title: "Oh My Mind | modernlove | EP",
-    image: '/assets/albums/modernlove_oh_my_mind.png',
+    image: '/assets/albums/modernlove_oh_my_mind.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2022', 'genre_indie']
   },
   {
     title: "Nightlife | modernlove | EP",
-    image: '/assets/albums/modernlove_nightlife.png',
+    image: '/assets/albums/modernlove_nightlife.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2023', 'genre_indie']
   },
   {
     title: "Yours | modernlove | Single",
-    image: '/assets/albums/modernlove_yours.png',
+    image: '/assets/albums/modernlove_yours.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2024', 'genre_indie']
   },
   {
     title: "Don't Feel Myself | modernlove | Single",
-    image: '/assets/albums/modernlove_dont_feel_myself.png',
+    image: '/assets/albums/modernlove_dont_feel_myself.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2024', 'genre_indie']
   },
   {
     title: "King of My Head | Declan Welsh and The Decadent West | Single",
-    image: '/assets/albums/declan_welsh_king_of_my_head.png',
+    image: '/assets/albums/declan_welsh_king_of_my_head.webp',
     filters: ['contribution_mixer', 'year_2023', 'genre_rock']
   },
   {
     title: "Mercy | Declan Welsh and The Decadent West | Single",
-    image: '/assets/albums/declan_welsh_mercy.png',
+    image: '/assets/albums/declan_welsh_mercy.webp',
     filters: ['contribution_mixer', 'year_2022', 'genre_rock']
   },
   {
     title: 'Before You Go (Orchestral) | Lewis Capaldi | Single',
-    image: '/assets/albums/lewis_capaldi_before_you_go.png',
+    image: '/assets/albums/lewis_capaldi_before_you_go.webp',
     filters: ['contribution_mixer', 'contribution_production', 'year_2020', 'genre_pop']
   },
   {
     title: 'Bruises (Acoustic) | Lewis Capaldi | Single',
-    image: '/assets/albums/lewis_capaldi_bruises.png',
+    image: '/assets/albums/lewis_capaldi_bruises.webp',
     filters: ['contribution_mixer', 'contribution_production', 'year_2019', 'genre_pop']
   },
   {
     title: "AUSTRIA | Glazed Curtains | Single",
-    image: '/assets/albums/glazed_curtains_austria.png',
+    image: '/assets/albums/glazed_curtains_austria.webp',
     filters: ['contribution_mixer', 'year_2024', 'genre_rock']
   },
   {
     title: "ADHD FEVER | Glazed Curtains | Single",
-    image: '/assets/albums/glazed_curtains_adhd_fever.png',
+    image: '/assets/albums/glazed_curtains_adhd_fever.webp',
     filters: ['contribution_mixer', 'year_2024', 'genre_rock']
   },
   {
     title: "Still Dressed For Winter | Now Ex | EP",
-    image: '/assets/albums/now_ex_still_dressed_for_winter.png',
+    image: '/assets/albums/now_ex_still_dressed_for_winter.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2023', 'genre_pop']
   },
   {
     title: "Welcome To The Feeling | Now Ex | EP",
-    image: '/assets/albums/now_ex_welcome_to_the_feeling.png',
+    image: '/assets/albums/now_ex_welcome_to_the_feeling.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2024', 'genre_pop']
   },
   {
     title: "Live At St Pancras Old Church | Now Ex | Album",
-    image: '/assets/albums/now_ex_st_pancras.png',
+    image: '/assets/albums/now_ex_st_pancras.webp',
     filters: ['contribution_mixer', 'contribution_mastering', 'year_2024', 'genre_pop']
   },
   {
     title: "Sad Kids In America | Now Ex | EP",
-    image: '/assets/albums/now_ex_sad_kids_in_america.png',
+    image: '/assets/albums/now_ex_sad_kids_in_america.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2021', 'genre_pop']
   },
   {
     title: "In Groede | Gana | Album",
-    image: '/assets/albums/gana_in_groede.png',
+    image: '/assets/albums/gana_in_groede.webp',
     filters: ['contribution_mixer', 'contribution_production', 'year_2022', 'genre_indie']
   },
   {
     title: "Take Cover | Neck&Neck | Single",
-    image: '/assets/albums/neck_and_neck_take_cover.png',
+    image: '/assets/albums/neck_and_neck_take_cover.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'contribution_writer', 'year_2022', 'genre_rock']
   },
   {
     title: "Call It What You Like | Neck&Neck | Single",
-    image: '/assets/albums/neck_and_neck_call_it_what_you_like.png',
+    image: '/assets/albums/neck_and_neck_call_it_what_you_like.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'contribution_writer', 'year_2022', 'genre_rock']
   },
   {
     title: "Blindside| Neck&Neck | Single",
-    image: '/assets/albums/neck_and_neck_blindside.png',
+    image: '/assets/albums/neck_and_neck_blindside.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'contribution_writer', 'year_2021', 'genre_rock']
   },
   {
     title: "Doctor | HAWKE THE BAND | Single",
-    image: '/assets/albums/hawke_the_band_doctor.png',
+    image: '/assets/albums/hawke_the_band_doctor.webp',
     filters: ['contribution_recording-enngineer', 'contribution_mixer', 'contribution_production', 'year_2024', 'genre_indie']
   },
   {
     title: "Proposal | HAWKE THE BAND | Single",
-    image: '/assets/albums/hawke_the_band_proposal.png',
+    image: '/assets/albums/hawke_the_band_proposal.webp',
     filters: ['contribution_recording-enngineer', 'contribution_mixer', 'contribution_production', 'year_2024', 'genre_indie']
   },
   {
     title: "Scraps In The Bowl | HAWKE THE BAND | Single",
-    image: '/assets/albums/hawke_the_band_scraps_in_the_bowl.png',
+    image: '/assets/albums/hawke_the_band_scraps_in_the_bowl.webp',
     filters: ['contribution_recording-enngineer', 'contribution_mixer', 'contribution_production', 'year_2024', 'genre_indie']
   },
   {
     title: "Bellicose Grounds | SUNKIN | Single",
-    image: '/assets/albums/sunkin_bellicose_grounds.png',
+    image: '/assets/albums/sunkin_bellicose_grounds.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2023', 'genre_indie']
   },
   {
     title: "The Pond | SUNKIN | Single",
-    image: '/assets/albums/sunkin_the_pond.png',
+    image: '/assets/albums/sunkin_the_pond.webp',
     filters: ['contribution_mixer', 'contribution_producer', 'year_2021', 'genre_indie']
   },
   {
     title: "Soma | Branko | Album",
-    image: '/assets/albums/branko_soma.png',
+    image: '/assets/albums/branko_soma.webp',
     filters: ['contribution_mixer', 'year_2024', 'genre_electronic']
   },
   {
     title: "Body | Tion Wayne | Single",
-    image: '/assets/albums/body_tion_wayne.png',
+    image: '/assets/albums/body_tion_wayne.webp',
     filters: ['contribution_recording_engineer', 'year_2021', 'genre_rap']
   },
   {
     title: "Kanadia | Kanadia | Album",
-    image: '/assets/albums/kanadia_kanadia.png',
+    image: '/assets/albums/kanadia_kanadia.webp',
     filters: ['contribution_mixer', 'contribution_recording_engineer', 'contribution_producer', 'year_2019', 'genre_rock']
   },
   {
     title: "June | Kanadia | Album",
-    image: '/assets/albums/kanadia_june.png',
+    image: '/assets/albums/kanadia_june.webp',
     filters: ['contribution_mixer', 'contribution_recording_engineer', 'contribution_producer', 'year_2022', 'genre_rock']
   },
   {
     title: "Garden | Exit Kid | Single",
-    image: '/assets/albums/exit_kid_garden.png',
+    image: '/assets/albums/exit_kid_garden.webp',
     filters: ['contribution_mixer', 'year_2020', 'genre_indie']
   },
   {
     title: "Time 2 Be | Codyy | Single",
-    image: '/assets/albums/cody_time_2_be.png',
+    image: '/assets/albums/cody_time_2_be.webp',
     filters: ['contribution_mixer', 'contribution_producer','year_2023', 'genre_indie']
   },
   {
     title: "Here I Go | Mulimba | EP",
-    image: '/assets/albums/mulimba_here_i_go.png',
+    image: '/assets/albums/mulimba_here_i_go.webp',
     filters: ['contribution_mixer', 'contribution_writer','contribution_producer', 'year_2021', 'genre_alt']
   },
   {
     title: "Live Like Me | Mulimba | Single",
-    image: '/assets/albums/mulimba_live_like_me.png',
+    image: '/assets/albums/mulimba_live_like_me.webp',
     filters: ['contribution_mixer', 'contribution_writer','contribution_producer', 'year_2022', 'genre_alt']
   },
   {
     title: "I'm Alive | Mulimba | Single",
-    image: '/assets/albums/mulimba_im_alive.png',
+    image: '/assets/albums/mulimba_im_alive.webp',
     filters: ['contribution_mixer', 'contribution_writer','contribution_producer', 'year_2022', 'genre_alt']
   },
   {
     title: "Need You Near | Andrew Dickson | EP",
-    image: '/assets/albums/andrew_dickson_need_you_near.png',
+    image: '/assets/albums/andrew_dickson_need_you_near.webp',
     filters: ['contribution_mixer', 'contribution_recording-engineer','contribution_producer', 'year_2022', 'genre_pop']
   },
   {
     title: "Siberia (ft Burna Boy) | Headie One | Single",
-    image: '/assets/albums/headie_one_siberia.png',
+    image: '/assets/albums/headie_one_siberia.webp',
     filters: ['contribution_recording-engineer', 'year_2021', 'genre_rap']
   },
   {
     title: "Hey Boy (ft Burna Boy) | Sia | Single",
-    image: '/assets/albums/hey_boy_sia.png',
+    image: '/assets/albums/hey_boy_sia.webp',
     filters: ['contribution_recording-engineer', 'year_2021', 'genre_pop']
   },
   {
     title: "Rotate | Burna Boy, Becky G | Single",
-    image: '/assets/albums/burna_boy_rotate.png',
+    image: '/assets/albums/burna_boy_rotate.webp',
     filters: ['contribution_recording-engineer', 'year_2021', 'genre_rap']
   },
   {
     title: "20 10 20 | Burna Boy | Single",
-    image: '/assets/albums/burna_boy_20_10_20.png',
+    image: '/assets/albums/burna_boy_20_10_20.webp',
     filters: ['contribution_recording-engineer', 'year_2020', 'genre_afrobeat']
   },
   {
     title: "Once Kings | Riz Ahmed | Single",
-    image: '/assets/albums/riz_ahmed_once_kings.png',
+    image: '/assets/albums/riz_ahmed_once_kings.webp',
     filters: ['contribution_recording-engineer', 'year_2020', 'genre_rap']
   },
   {
     title: "I'm Not Coming Home | Hector Cottam | Single",
-    image: '/assets/albums/hector_cottam_im_not_coming_home.png',
+    image: '/assets/albums/hector_cottam_im_not_coming_home.webp',
     filters: ['contribution_producer', 'contribution_mastering', 'contribution_mixer', 'year_2020', 'genre_rock']
   }
 ];
