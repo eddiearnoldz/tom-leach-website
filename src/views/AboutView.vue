23123c4a94165ce653bb6c2d6132d446d5175b1b
eddie arnold <eddiearnoldz@mac.com>
2024-07-26 13:40:22 +0100

    added image to about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -5,6 +5,7 @@
     <p>Over my career so far, I've been fortunate to have  worked with artists and producers like Fergie, Lewis Capaldi, Burna Boy, Headie One, Pa Salieu, Tion Wayne, Wheatus, Shy FX, Mr Scruff, FKA Twigs, Years and Years, Stormzy, Wretch 32, Clean Bandit, Deaf Havana, Spiritualized, Beth Gibbons, James Ford, Jerry Boys, Haydn Bendall and many, many others.</p>
     <p>I'm at home working on everything from Rock to EDM, hip hop to classical to big band and everything in between. Check out some of my work on the next page.</p>
     <p>I'm one half of 9 Pockets, a production duo working on everything from major label releases to sync and score composition. More info on 9 Pockets here: <a href="https://9pockets.co.uk/" target="_blank">https://9pockets.co.uk/</a></p>
+    <img src="/assets/profile_picture.webp" alt="Tom in the studio">
   </div>
 </template>
 
@@ -26,6 +27,13 @@
   .about a {
     color: #1C9BEF;
     text-decoration: none;
+  }
+
+  .about img {
+    max-width: 500px;
+    margin: 50px;
+    border-radius: 2px;
+    width: 100%;
   }
 
   @media screen and (min-width: 768px) {
