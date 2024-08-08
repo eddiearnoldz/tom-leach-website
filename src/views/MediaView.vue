4ff636a32879237918a93d4b9f7bf0db0098bf02
eddie arnold <eddiearnoldz@mac.com>
2024-08-08 13:50:41 +0100

    changed copy on media page

--- src/views/MediaView.vue
+++ src/views/MediaView.vue
@@ -2,7 +2,7 @@
   <div class="media container">
     <h1>MEDIA</h1>
     <p>Below is a playlist of songs I have worked on.</p>
-    <p>Please feel free to contact me if you are looking for a more in depth catalogue of my work, or for a CV.</p>
+    <p>Please contact me if you are looking for a more in depth catalogue of my work.</p>
     <div class="spotify_embed">
       <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/7fonVzgZw9enlvRlztIduw?utm_source=generator" width="100%" height="352" frameBorder="0"  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
